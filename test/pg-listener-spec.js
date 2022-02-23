ba71118f6c07f4848d8f2c4df9bb9fdd41ca23c3
meenahoda <meena.hoda@wmfs.net>
2022-02-23 13:29:27 +0000

    test: more inserts/deletes tests

--- test/pg-listener-spec.js
+++ test/pg-listener-spec.js
@@ -22,7 +22,7 @@ describe('PG listener service tests', function () {
   let star
 
   const received = []
-  let expectedReceived = 0
+  let expectedTotalReceived = 0
 
   before(function () {
     if (process.env.PG_CONNECTION_STRING && !/^postgres:\/\/[^:]+:[^@]+@(?:localhost|127\.0\.0\.1).*$/.test(process.env.PG_CONNECTION_STRING)) {
@@ -66,28 +66,66 @@ describe('PG listener service tests', function () {
 
       ws.onopen = event => {
         ws.onmessage = async msg => {
-          received.push(msg.data)
+          received.push(JSON.parse(msg.data))
         }
       }
     })
   })
 
   describe('After insert model trigger', () => {
-    it('insert some data', async () => {
-      await people.create({
-        employeeNo: 3,
-        firstName: 'Santa',
-        lastName: 'Claus'
-      })
+    const key = 'tymly_test_people_afterInsert'
+    let expectedReceived = 0
+
+    it('insert a record', async () => {
+      await people.create({ employeeNo: 3, firstName: 'Santa', lastName: 'Claus' })
+      expectedTotalReceived++
       expectedReceived++
     })
 
     it('wait', done => setTimeout(done, 300))
 
     it('check received updates', () => {
-      expect(received.length).to.eql(expectedReceived)
-      const { key, record } = JSON.parse(received[received.length - 1])
-      expect(key).to.eql('tymly_test_people_afterInsert')
+      expect(received.length).to.eql(expectedTotalReceived)
+      expect(received.filter(r => r.key === key).length).to.eql(expectedReceived)
+
+      const { record } = received[received.length - 1]
+      expect(record.employee_no).to.eql(3)
+    })
+
+    it('insert multiple records', async () => {
+      await people.create([
+        { employeeNo: 4, firstName: 'Santa 1', lastName: 'Claus' },
+        { employeeNo: 5, firstName: 'Santa 2', lastName: 'Claus' }
+      ])
+      expectedTotalReceived += 2
+      expectedReceived += 2
+    })
+
+    it('wait again', done => setTimeout(done, 300))
+
+    it('check received updates for additional records', () => {
+      expect(received.length).to.eql(expectedTotalReceived)
+      expect(received.filter(r => r.key === key).length).to.eql(expectedReceived)
+    })
+  })
+
+  describe('After delete model trigger', () => {
+    const key = 'tymly_test_people_afterDelete'
+    let expectedReceived = 0
+
+    it('delete a record', async () => {
+      await people.destroyById(3)
+      expectedTotalReceived++
+      expectedReceived++
+    })
+
+    it('wait', done => setTimeout(done, 300))
+
+    it('check received updates', () => {
+      expect(received.length).to.eql(expectedTotalReceived)
+      expect(received.filter(r => r.key === key).length).to.eql(expectedReceived)
+
+      const { record } = received[received.length - 1]
       expect(record.employee_no).to.eql(3)
     })
   })
